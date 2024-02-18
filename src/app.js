0b8b128a38abd762438f04e437988506de6704e4
Freakydots <sarthakgupta150603@gmail.com>
2024-02-18 17:33:43 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))//url
 app.use(express.static("public"))//image
 app.use(cookieParser())//user ki browser cookies pr crud operations 
 
-export default app
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)
+
+export {app}
